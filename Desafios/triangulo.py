b810f3797e6ce42413724620029a76697e95c14b
LuckScarpelli <73.scar@gmail.com>
2022-05-10 10:18:37 -0300

    inserindo arquivo triangulo.py

--- Desafios/triangulo.py
+++ Desafios/triangulo.py
@@ -1,1 +1,33 @@
+import sys
+
+def is_triangle(*args): # validade triangle
+   if sum(sides[:-1]) <= sides[-1]:
+      return False
+      
+
+def which_triangle(*args): # define triangle
+   if (sides[0] == sides[1] and sides[1] == sides[-1]):
+      return 1
+   elif (sides[0] == sides[1] or sides[1] == sides[-1] or sides[0] == sides[-1]):
+      return 2 
+      
+
+side = ""
+sides = []
+
+for lines in range(1,4): # reading three sides
+    try:
+       side = int(input(f"Enter with side {lines}: "))
+    except ValueError:
+        print("Invalid value, type a number")
+        sys.exit(1)
+    sides.append(side)
+sides.sort()
+
+if is_triangle(which_triangle(sides)) == False:
+    print('\n Is not a Triangle')
+    quit()
+if which_triangle(sides) == 1: print("\n equilateral triangle")
+elif which_triangle(sides) == 2: print("\n isosceles triangle")
+else: print("\n scalene triangle")
 
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ## Repositório de resolução de desafios em Python
 
+- triangulo.py - scripty que retorna se as medidas informadas formam um triangulo, se formarem, exibe qual triangulo é formado.
